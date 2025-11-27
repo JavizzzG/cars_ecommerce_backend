03a0220a6653032048e458f193b49621ef2f3867
JavizzzG <javierstivengalvis2008@gmail.com>
2025-11-27 06:14:12 -0500

    First commit

---
+++ src/test/java/com/javiz/cars_ecommerce_backend/CarsEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javiz.cars_ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
